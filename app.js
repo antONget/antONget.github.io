8b735a83db15f517ad013f9f5aab7f0d6dc9ed60
antONget <64537429+antONget@users.noreply.github.com>
2023-09-12 21:47:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -95,7 +95,12 @@ ${tg.initDataUnsafe.user.last_name}`;
 
 usercard.appendChild(p);
 
-
+function flipCard(event) {
+  const flipCardContainer = event.currentTarget.querySelector(".flip-card-container")
+  flipCardContainer.classList.toggle("flip-card")
+}
+const card = document.querySelector(".card")
+card.addEventListener("click",event=>flipCard(event))
 
 
 
